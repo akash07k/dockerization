be84a4e78f77b3f286a339ffaec8be663398627a
Akash Kakkar <6381747+akash07k@users.noreply.github.com>
2023-08-31 18:38:43 +0530

    nextcloud: Back up the database as it's inefficient to only back up its volumes

--- scripts/post/nextcloud.sh
+++ scripts/post/nextcloud.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source "$SCRIPT_DIR/../functions.sh"
 
 # Define an array of container names
-containers=("nextcloud-app" "nextcloud-db")
+containers=("nextcloud-app")
 
 # Iterate through the container names and start each container
 for container in "${containers[@]}"; do
