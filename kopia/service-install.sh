5a13634dd304f6d892f9f6df1a523af5b76e16d7
Akash Kakkar <6381747+akash07k@users.noreply.github.com>
2023-09-02 22:59:37 +0530

    kopia: Correctly set the persistent env vars in service-install script and don't escape them in service execution

--- kopia/service-install.sh
+++ kopia/service-install.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-# Prompt the user for the Kopia server username and password
-read -p "Enter Kopia server username: " KOPIA_USERNAME
-read -s -p "Enter Kopia server password: " KOPIA_PASSWORD
-echo
+# Check if KOPIA_USERNAME and KOPIA_PASSWORD are already set
+if [ -z "$KOPIA_USERNAME" ] || [ -z "$KOPIA_PASSWORD" ]; then
+    # Prompt the user for the Kopia server username and password
+    read -p "Enter Kopia server username: " KOPIA_USERNAME
+    read -s -p "Enter Kopia server password: " KOPIA_PASSWORD
+    echo
+
+    # Add the environment variables to ~/.bashrc
+    echo "export KOPIA_USERNAME=\"$KOPIA_USERNAME\"" >> ~/.bashrc
+    echo "export KOPIA_PASSWORD=\"$KOPIA_PASSWORD\"" >> ~/.bashrc
+
+    # Reload ~/.bashrc to make the variables available in the current session
+    source ~/.bashrc
+else
+    echo "KOPIA_USERNAME and KOPIA_PASSWORD are already set."
+fi
 
 # Create a systemd service unit file
-cat <<EOF > /etc/systemd/system/kopia-server.service
+sudo tee /etc/systemd/system/kopia-server.service <<EOF
 [Unit]
 Description=Kopia Server Service
 
@@ -15,18 +27,18 @@ ExecStart=/usr/bin/kopia server start --address http://0.0.0.0:51515 --server-us
 Restart=always
 User=akash
 WorkingDirectory=/home/akash
-Environment="KOPIA_USERNAME=$KOPIA_USERNAME" "KOPIA_PASSWORD=$KOPIA_PASSWORD"
+Environment="KOPIA_USERNAME=\$KOPIA_USERNAME" "KOPIA_PASSWORD=\$KOPIA_PASSWORD"
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
 # Reload systemd configuration
-systemctl daemon-reload
+sudo systemctl daemon-reload
 
 # Enable and start the service
-systemctl enable kopia-server.service
-systemctl start kopia-server.service
+sudo systemctl enable kopia-server.service
+sudo systemctl start kopia-server.service
 
 # Print service status
-systemctl status kopia-server.service
+sudo systemctl status kopia-server.service
