235e635eb841b39c5d2f5ee50705c4e84a27c4ac
Akash Kakkar <6381747+akash07k@users.noreply.github.com>
2023-09-03 12:18:30 +0530

    kopia: Do not use username and password

--- kopia/service-install.sh
+++ kopia/service-install.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-# Check if KOPIA_SERVER_USERNAME and KOPIA_SERVER_PASSWORD are already set
-if [ -z "$KOPIA_SERVER_USERNAME" ] || [ -z "$KOPIA_SERVER_PASSWORD" ]; then
-    # Prompt the user for the Kopia server username and password
-    read -p "Enter Kopia server username: " KOPIA_SERVER_USERNAME
-    read -s -p "Enter Kopia server password: " KOPIA_SERVER_PASSWORD
-    echo
-
-    # Add the environment variables to ~/.bashrc
-    echo "export KOPIA_SERVER_USERNAME=\"$KOPIA_SERVER_USERNAME\"" >> ~/.bashrc
-    echo "export KOPIA_SERVER_PASSWORD=\"$KOPIA_SERVER_PASSWORD\"" >> ~/.bashrc
-
-    # Reload ~/.bashrc to make the variables available in the current session
-    source ~/.bashrc
-else
-    echo "KOPIA_SERVER_USERNAME and KOPIA_SERVER_PASSWORD are already set."
-fi
-
 # Create a systemd service unit file
 sudo tee /etc/systemd/system/kopia-server.service <<EOF
 [Unit]
@@ -25,7 +8,7 @@ Description=Kopia Server Service
 [Service]
 ExecStartPre=/bin/bash -c 'sudo setcap cap_dac_read_search=+ep /usr/bin/kopia'
 ExecStartPre=/bin/bash -c 'sudo setcap cap_dac_override=+ep /usr/bin/kopia'
-ExecStart=kopia server start --address http://0.0.0.0:51515 --server-username=$KOPIA_SERVER_USERNAME --server-password=$KOPIA_SERVER_PASSWORD --enable-actions --refresh-interval=24h --insecure
+ExecStart=kopia server start --address http://0.0.0.0:51515 --enable-actions --refresh-interval=24h --without-password --insecure
 Restart=always
 User=akash
 WorkingDirectory=/home/akash
