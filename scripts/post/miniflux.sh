eea1840dfe9c4d618b0968f9d9d691ca7c6f14c4
Akash Kakkar <6381747+akash07k@users.noreply.github.com>
2023-09-03 16:56:15 +0530

    scripts: Update the message for post backup scripts

--- scripts/post/miniflux.sh
+++ scripts/post/miniflux.sh
@@ -11,5 +11,5 @@ containers=("miniflux-app")
 
 # Iterate through the container names and start each container
 for container in "${containers[@]}"; do
-    start_container "$container" true "post-backup" "low" "Container '$container' has been started."
+    start_container "$container" true "post-backup" "low" "Container '$container' backup is complete. Container has been started."
 done
